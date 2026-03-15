f9e3e553929c78f6785964c89569133f823d7df1
augustus21adi <96939401+augustus21adi@users.noreply.github.com>
2026-03-15 15:50:20 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,22 @@
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse
+	SET SINGLE_USER
+	WITH ROLLBACK IMMEDIATE;
+
+	DROP DATABASE DataWarehouse;
+END
+
+CREATE DATABASE DataWarehouse;
+
+
+
+USE DataWarehouse;
+
+
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
